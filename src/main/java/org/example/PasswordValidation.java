5cb8acee2a8e7afac9164f06d918274f6ac68ddd
Robert Stolz <robert.stolz87@gmail.com>
2024-10-01 18:03:47 +0200

    Alle Aufgaben inkl. Bonus

---
+++ src/main/java/org/example/PasswordValidation.java
@@ -0,0 +1,116 @@
+package org.example;
+
+public class PasswordValidation {
+    public static void main(String[] args) {
+         System.out.println("Aufgabe 1: " + moreThanEightChars("huhuhuhuhuhuhu"));
+         System.out.println("Aufgabe 2: " + pwWithDigits("11hu"));
+         System.out.println("Aufgabe 3: " + hasUpperAndLowerChars("Ddfuhu"));
+         System.out.println("Aufgabe 4: " + pwIsNotCommonlyUsed("1234"));
+         System.out.println("Aufgabe 5: " + pwContainsSpecialCharacters("$adsad$$$"));
+         System.out.println("Aufgabe 6: " + createUniquePw());
+
+    }
+
+    public static boolean moreThanEightChars(String password) {
+        return password.length() > 8;
+    }
+
+    public static boolean pwWithDigits(String password){
+        // return password.matches("^(?=.*\\d).+$");
+        for (int i = 0; i < password.length(); i++) {
+            char charToCheck = password.charAt(i);
+            if (Character.isDigit(charToCheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static boolean hasUpperAndLowerChars(String password){
+        // return password.matches("^(?=.*[a-z])(?=.*[A-Z]).+$");
+        boolean hasUpper = false;
+        boolean hasLower = false;
+
+        for(int i = 0; i < password.length(); i++){
+            char charToCheck = password.charAt(i);
+            if (Character.isUpperCase(charToCheck)) {
+                hasUpper = true;
+            } else if (Character.isLowerCase(charToCheck)) {
+                hasLower = true;
+            }if (hasUpper && hasLower) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean pwIsNotCommonlyUsed(String password){
+        //return password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\\]).{8,32}$");
+        //At least one digit [0-9] At least one lowercase character [a-z] At least one uppercase character [A-Z] At least one special character [*.!@#$%^&(){}[]:;<>,.?/~_+-=|\] At least 8 characters in length, but no more than 32.
+        String[] commonPasswords = {"1234", "abcd", "qwer"};
+        for (String commonPassword : commonPasswords) {
+            if (password.equals(commonPassword)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean pwContainsSpecialCharacters(String password){
+        String[] specialCharacters = { "*", ".", "!", "@", "#", "$", "%", "^", "&", "(", ")", "{", "}", "[", "]", ":", ";", "<", ">", ",", ".", "?", "/", "~", "_", "+", "-", "=", "|", "\\" };
+        for (String specialCharacter : specialCharacters){
+            for(int k = 0; k < password.length(); k++ ){
+                if(Character.toString(password.charAt(k)).equals(specialCharacter)){
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public static String createUniquePw(){
+
+        String[] specialCharacters = { "*", ".", "!", "@", "#", "$", "%", "^", "&", "(", ")", "{", "}", "[", "]", ":", ";", "<", ">", ",", ".", "?", "/", "~", "_", "+", "-", "=", "|", "\\" };
+        String[] digits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        String[] lowercaseAlphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        String[] uppercaseAlphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        String password = "";
+
+        for(int i = 1; i < 6; i++){
+            int randomIndex = (int) (Math.random() * specialCharacters.length);
+            password += specialCharacters[randomIndex];;
+        }
+
+        for(int i = 1; i < 6; i++){
+            int randomIndex = (int) (Math.random() * digits.length);
+            password += digits[randomIndex];;
+        }
+
+        for(int i = 1; i < 6; i++){
+            int randomIndex = (int) (Math.random() * lowercaseAlphabet.length);
+            password += lowercaseAlphabet[randomIndex];;
+        }
+
+        for(int i = 1; i < 6; i++){
+            int randomIndex = (int) (Math.random() * uppercaseAlphabet.length);
+            password += uppercaseAlphabet[randomIndex];;
+        }
+        //String rndPassword = shuffleString(password);
+
+        char[] passwordArray = password.toCharArray();
+        for (int i = 0; i < passwordArray.length; i++) {
+            int randomIndex = (int) (Math.random() * passwordArray.length);
+            char temp = passwordArray[i];
+            passwordArray[i] = passwordArray[randomIndex];
+            passwordArray[randomIndex] = temp;
+        }
+
+        return new String(passwordArray);
+
+        // warum geht nicht .sample(5) & .shuffle.... *heul*
+    }
+
+
+}
---
+++ src/test/java/PasswordValidationTest.java
@@ -0,0 +1,122 @@
+import org.example.PasswordValidation;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PasswordValidationTest {
+    @Test
+    void checkMoreThanEightChars(){
+    //given
+        String password = "wer das liesst, kann lesen";
+    //when
+        boolean actual = PasswordValidation.moreThanEightChars(password);
+    //then
+        assertTrue(actual);
+    }
+
+    @Test
+    void checkLessThanEightChars() {
+        // given
+        String password = "kurz";
+
+        // when
+        boolean actual = PasswordValidation.moreThanEightChars(password);
+
+        // then
+        assertFalse(actual);
+    }
+
+    @Test
+    void checkIfContainsDigit(){
+        // given
+        String password = "hallo123";
+
+        //when
+        boolean actual = PasswordValidation.pwWithDigits(password);
+
+        //then
+        assertTrue(actual);
+    }
+    @Test
+    void checkIfContainsNoDigit(){
+        // given
+        String password = "hallo";
+
+        //when
+        boolean actual = PasswordValidation.pwWithDigits(password);
+
+        //then
+        assertFalse(actual);
+    }
+    @Test
+    void checkIfContainsUpperAndLowerChars(){
+        // given
+        String password = "Hallo";
+
+        //when
+        boolean actual = PasswordValidation.hasUpperAndLowerChars(password);
+
+        //then
+        assertTrue(actual);
+    }
+    @Test
+    void checkIfNoContainsUpperAndLowerChars(){
+        // given
+        String password = "hallo";
+
+        //when
+        boolean actual = PasswordValidation.hasUpperAndLowerChars(password);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    void checkIfPwIsCommon(){
+        //given
+        String password = "1234";
+
+        //when
+        boolean actual = PasswordValidation.pwIsNotCommonlyUsed(password);
+
+        //then
+        assertFalse(actual);
+    }
+
+    @Test
+    void checkIfPwIsNotCommon(){
+        //given
+       String password = "RqByMEthf4w&";
+
+       //when
+       boolean actual = PasswordValidation.pwIsNotCommonlyUsed(password);
+
+       //then
+       assertTrue(actual);
+    }
+
+    @Test
+    void checkIfPwContainsSpecialCharacters(){
+        //given
+        String password = "RqByMEthf4w&";
+
+        //when
+        boolean actual = PasswordValidation.pwContainsSpecialCharacters(password);
+
+        //then
+        assertTrue(actual);
+    }
+
+    @Test
+    void checkIfPwNotContainsSpecialCharacters(){
+        //given
+        String password = "huhu";
+
+        //when
+        boolean actual = PasswordValidation.pwContainsSpecialCharacters(password);
+
+        //then
+        assertFalse(actual);
+    }
+}
